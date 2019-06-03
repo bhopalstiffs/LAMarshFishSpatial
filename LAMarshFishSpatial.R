44ebcf048ef9b15cc566313eeff87ff55743e3dd
bhopalstiffs <49293030+bhopalstiffs@users.noreply.github.com>
2019-06-03 14:51:07 -0700

    Add files via upload

---
+++ LAMarshFishSpatial.R
@@ -0,0 +1,8 @@
+#Removal estimates for fish species
+
+library(FSA)
+
+# Load "FishData.xlsx"
+# Create a loop that will go through each row in the "FishData.xlsx" and calculate this function:
+# removal(row vector, method = "Burnham", just.ests = TRUE), results will go in the total column in the "FishData.xlsx"
+
