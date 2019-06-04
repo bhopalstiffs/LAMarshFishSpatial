7daaae754edfff6e0ebf27bcf2d91fe5ee214f49
bhopalstiffs <49293030+bhopalstiffs@users.noreply.github.com>
2019-06-04 14:02:25 -0700

    Add files via upload

--- LAMarshFishSpatial.R
+++ LAMarshFishSpatial.R
@@ -4,5 +4,5 @@ library(FSA)
 
 # Load "FishData.xlsx"
 # Create a loop that will go through each row in the "FishData.xlsx" and calculate this function:
-# removal(row vector, method = "Burnham", just.ests = TRUE), results will go in the total column in the "FishData.xlsx"
+# removal(data = row vector(J-S or Pass1-Pass10), method = "Burnham", just.ests = TRUE), results will go in the total column in the "FishData.xlsx"
 
